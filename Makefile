30d2c4d510f346555a99d0568295773b41da62e7
James Philip Iddon <jamesiddon94@gmail.com>
2023-06-20 14:36:01 +0200

    Github pages deployment updates

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
-all: jpi_cv.html jpi_cv.pdf jpi_cv.docx jpi_cv.txt
+all: jpi_cv.html jpi_cv.pdf jpi_cv.txt
 
 jpi_cv.html: jpi_cv.md style.css
 	pandoc --standalone -c style.css --from markdown --to html -o jpi_cv.html jpi_cv.md
-	./update_web.sh
 
 jpi_cv.pdf: jpi_cv.html
 	wkhtmltopdf --enable-local-file-access jpi_cv.html jpi_cv.pdf
 
-jpi_cv.docx: jpi_cv.md
-	pandoc --from markdown --to docx -o jpi_cv.docx jpi_cv.md
-
 jpi_cv.txt: jpi_cv.md
 	pandoc --standalone -f markdown-smart --from markdown --to plain -o jpi_cv.txt jpi_cv.md
+
+update_web:
+	./update_web.sh
 
 clean:
 	rm -f *.html *.pdf *.docx *.txt *~
--- update_web.sh
+++ update_web.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-echo "cp jpi_cv.html style.css ../jiddon.github.io/"
-cp jpi_cv.html style.css ../jiddon.github.io/
-echo "cd ../jiddon.github.io/"
+cp jpi_cv.html ../jiddon.github.io/
+cp style.css ../jiddon.github.io/
+cp jpi_cv.pdf ../jiddon.github.io/
+cp jpi_cv.txt ../jiddon.github.io/
+
 cd ../jiddon.github.io/
-echo "git add jpi_cv.html style.css"
-git add jpi_cv.html style.css
-echo "git commit -m "Update""
+
+head -n -2 jpi_cv.html > index.html # remove </body> and </html>
+rm jpi_cv.html
+echo "<h3 id="view-in-pdf-txt">View in <a href="./jpi_cv.pdf">pdf</a>, <a href="./jpi_cv.txt">txt</a></h3>" >> index.html
+echo "</body>" >> index.html
+echo "</html>" >> index.html
+
+git add index.html style.css jpi_cv.pdf jpi_cv.txt
 git commit -m "Update"
-echo "git push origin main"
 git push origin main
-echo "cd -"
 cd -
