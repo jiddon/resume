17bce6882ad193458437781811ad3d426478d7d4
James Philip Iddon <james.philip.iddon@cern.ch>
2024-07-30 18:00:41 +0200

    fix github pages script

--- update_web.sh
+++ update_web.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+echo '```' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
+echo '---' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
+echo 'author_profile: true' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
+echo 'permalink: /cv/cv_txt/' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
+echo 'layout: archive' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
+echo '---' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
+echo '```' >> jpi_cv.txt
+
 cp jpi_cv.pdf ../jiddon.github.io/files/jpi_cv.pdf
+cp jpi_cv.txt ../jiddon.github.io/_pages/cv_txt.md
 
 cd ../jiddon.github.io/
 
