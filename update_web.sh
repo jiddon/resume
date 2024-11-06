8a854bf46de6b94c14396c7b5e1c21fb604e2230
James Philip Iddon <james.philip.iddon@cern.ch>
2024-11-06 21:52:08 +0100

    extend script to new web build

--- update_web.sh
+++ update_web.sh
@@ -7,14 +7,29 @@ echo 'layout: archive' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
 echo '---' | cat - jpi_cv.txt > temp && mv temp jpi_cv.txt
 echo '```' >> jpi_cv.txt
 
+cat jpi_cv.md | tail -n +5 > cv.md
+echo '---' | cat - cv.md > temp && mv temp cv.md
+echo 'author_profile: true' | cat - cv.md > temp && mv temp cv.md
+echo 'permalink: /cv/' | cat - cv.md > temp && mv temp cv.md
+echo 'layout: archive' | cat - cv.md > temp && mv temp cv.md
+echo 'title: James Philip Iddon' | cat - cv.md > temp && mv temp cv.md
+echo '---' | cat - cv.md > temp && mv temp cv.md
+echo ' ' >> cv.md
+echo '---' >> cv.md
+echo ' ' >> cv.md
+echo 'download as [pdf](../files/jpi_cv.pdf) or view in [txt](./cv_txt)' >> cv.md
+
+
 cp jpi_cv.pdf ../jiddon.github.io/files/jpi_cv.pdf
 cp jpi_cv.txt ../jiddon.github.io/_pages/cv_txt.md
+cp cv.md ../jiddon.github.io/_pages/cv.md
 
 cd ../jiddon.github.io/
 
 if [[ $# == 1 && $1 == "push" ]]; then
   git add files/jpi_cv.pdf 
   git add _pages/cv_txt.md
+  git add _pages/cv.md
   git commit -m "Update cv"
   git push
 else
